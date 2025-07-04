81ab5e831fa57256c8c493f0fc8b7815ee4093b7
Manaf <manaf18.m@gmail.com>
2025-07-04 20:55:56 +0000

    add active status to cohort

--- backend/internships/models.py
+++ backend/internships/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from django.db import models
 from datetime import datetime, timedelta
 from django.utils import timezone
@@ -7,6 +8,17 @@ class Cohort(models.Model):
     year = models.CharField(max_length=20, unique=True)
     start_date = models.DateField(default=timezone.now)
     end_date = models.DateField(default=datetime.now() + timedelta(days=120))
+    active = models.BooleanField(default=True)
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ("upcoming", "Upcoming"),
+            ("ongoing", "Ongoing"),
+            ("completed", "Completed"),
+            ("cancelled", "Cancelled"),
+        ],
+        default="ongoing",
+    )
 
     def __str__(self):
         return f"{self.year} internship cohort"
